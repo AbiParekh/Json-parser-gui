8c210d09c93339fc669125e8c1a61d1cdce0dcbf
Abhishek Parekh <abhishekparekh25@yahoo.ca>
2023-09-12 12:30:38 -0400

    added front-end GUI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [jsonData, setJsonData] = useState("");   // Initialize with an empty string
+    const [error, setError] = useState(null);       // State to handle any errors
+
+    const fetchData = async () => {
+        try {
+            let response = await fetch("http://127.0.0.1:5000/get_data");
+            const contentType = response.headers.get("content-type");
+
+            if (response.headers.get("Content-Type").includes("application/json")) {
+                let data = await response.json();
+                setJsonData(JSON.stringify(data, null, 4));
+            } else {
+                let errorText = await response.text();
+                throw new Error(`Unexpected response: ${errorText}`);
+            }
+        } catch (error) {
+            console.error("There was an error fetching the data", error);
+            setError(error.toString());
+        }
+    };
+
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={fetchData}>Fetch JSON Data</button>
+                <pre>{jsonData}</pre>
+                {error && <p style={{ color: "red" }}>{error}</p>}   {/* Display error if it exists */}
+            </header>
+        </div>
+    );
 }
 
 export default App;
